fe36102cbe66f5165464d4c0303cb791ef627d22
woooooooojin <kevin2205@naver.com>
2023-10-06 15:21:11 +0900

    loading animation

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -3,18 +3,67 @@ import { styled } from "styled-components"
 const Wrapper = styled.div`
     height: 100vh;
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
 `
 const Text = styled.span`
     font-size: 24px;
 `
+const LoadingWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .wave{
+    width: 3px;
+    height: 30px;
+    background: linear-gradient(45deg, powderblue, #fff);
+    margin: 10px;
+    animation: wave 1s linear infinite;
+    border-radius: 20px;
+  }
+  .wave:nth-child(2) {
+  animation-delay: 0.1s;
+  }
+  .wave:nth-child(3) {
+    animation-delay: 0.2s;
+  }
+  .wave:nth-child(4) {
+    animation-delay: 0.3s;
+  }
+  .wave:nth-child(5) {
+    animation-delay: 0.4s;
+  }
+
+    @keyframes wave {
+    0% {
+      transform: scale(0);
+    }
+    50% {
+      transform: scale(1);
+    }
+    100% {
+      transform: scale(0);
+    }
+}
+`
 
 
 export default function LoadingScreen() {
   return (
     <>
-        <Wrapper><Text>로딩중...</Text></Wrapper>
+        <Wrapper>
+          <Text>loading</Text>
+          <LoadingWrap>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+            <div className="wave"></div>
+          
+          </LoadingWrap>
+        </Wrapper>
+
     </>
   )
 }
